59608409c4c6ab7e664edd98cbe081b074fc201e
luisss3 <82846784+luisss3@users.noreply.github.com>
2025-05-13 22:15:19 -0400

    remove untracked files

--- build/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dylan-exo/motor_api/Exoskeleton-Embedded/googletest/googletest
-# Build directory: /home/dylan-exo/motor_api/Exoskeleton-Embedded/build/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
